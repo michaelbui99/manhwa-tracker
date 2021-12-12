b44b0123c79fece79b4d272f66a47af1ae48dccd
Michael Bui <michaelbui99@hotmail.com>
2021-12-12 22:13:47 +0100

    Added routing

--- ReactManhwaTracker/manhwa-tracker/src/App.js
+++ ReactManhwaTracker/manhwa-tracker/src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
 import "./App.css";
-
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/search" element={<Search />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
