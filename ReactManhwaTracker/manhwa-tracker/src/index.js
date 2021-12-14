1415675d2968349b026130065674fe2820f0d98a
Michael Bui <michaelbui99@hotmail.com>
2021-12-14 23:46:36 +0100

    Updated domain model, added ManhwaCard and SearchDirectory component

--- ReactManhwaTracker/manhwa-tracker/src/index.js
+++ ReactManhwaTracker/manhwa-tracker/src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { CssBaseline } from "@mui/material";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CssBaseline>
+      <App />
+    </CssBaseline>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- ReactManhwaTracker/manhwa-tracker/src/pages/search/Search.jsx
+++ ReactManhwaTracker/manhwa-tracker/src/pages/search/Search.jsx
@@ -1,9 +1,58 @@
 import * as React from "react";
-import { TextField } from "@mui/material";
+import { useState, useEffect } from "react";
+import {
+  Grid,
+  TextField,
+  FormLabel,
+  FormControl,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
+} from "@mui/material";
+import SearchDirectory from "../../components/search-directory/SearchDirectory";
 import InputAdornment from "@mui/material/InputAdornment";
 import SearchIcon from "@mui/icons-material/Search";
 import styles from "./Search.module.scss";
 const Search = () => {
+  const [manhwas, setManhwas] = useState([]);
+  const [manhwasToShow, setManhwasToShow] = useState([]);
+  const [genres, setGenres] = useState([
+    "Fantasy",
+    "Romance",
+    "Action",
+    "Comedy",
+  ]);
+  const [tags, setTags] = useState([]);
+  const [selectedGenres, setSelectedGenres] = useState([]);
+  const [selectedTags, setSelectedTags] = useState([]);
+  const testManhwa = {
+    id: 1,
+    title: "Solo leveling",
+    description:
+      "In a world where awakened beings called “Hunters” must battle deadly monsters to protect humanity, Sung Jinwoo, nicknamed “the weakest hunter of all mankind,” finds himself in a constant struggle for survival. One day, after a brutal encounter in an overpowered dungeon wipes out his party and threatens to end his life, a mysterious System chooses him as its sole player: Jinwoo has been granted the rare opportunity to level up his abilities, possibly beyond any known limits. Follow Jinwoo’s journey as he takes on ever-stronger enemies, both human and monster, to discover the secrets deep within the dungeons and the ultimate extent of his powers.",
+    format: "Manga (South Korean)",
+    status: "RELEASING",
+    sourceMaterial: "Web novel",
+    releaseDate: "12-12-2021",
+    endDate: "",
+    chapterCount: 120,
+    coverImage:
+      "https://s4.anilist.co/file/anilistcdn/media/manga/cover/large/bx105398-b673Vt5ZSuz3.jpg",
+    tags: [],
+    genres: [],
+  };
+  useEffect(() => {
+    //TODO: Fetch genres and tags on mount
+    const tempGenres = ["Fantasy", "Romance", "Action", "Comedy"];
+    const tempTags = ["Anti-Hero", "Super Power", "Anti-Hero"];
+    setGenres(tempGenres);
+    setTags(tempTags);
+    setManhwasToShow([testManhwa]);
+  }, []);
+
+  const selectGenre = (g) => {
+    setSelectedGenres(selectedGenres.concat(g));
+  };
   return (
     <div className={styles.container}>
       <div className={styles.search}>
@@ -21,6 +70,10 @@ const Search = () => {
             }}
           />
         </div>
+        <div className={styles.search__filters}></div>
+      </div>
+      <div className={styles.search__directory}>
+        <SearchDirectory manhwas={manhwasToShow} />
       </div>
     </div>
   );
