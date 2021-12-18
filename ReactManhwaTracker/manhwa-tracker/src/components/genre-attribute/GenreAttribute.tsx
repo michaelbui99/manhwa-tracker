855f6139af964541c2b01230550e05e63a10b0ec
Michael Bui <michaelbui99@hotmail.com>
2021-12-18 02:01:47 +0100

    refractor GenreAttribute to use ChakraUI Badge

--- ReactManhwaTracker/manhwa-tracker/src/components/genre-attribute/GenreAttribute.tsx
+++ ReactManhwaTracker/manhwa-tracker/src/components/genre-attribute/GenreAttribute.tsx
@@ -1,24 +1,12 @@
 import * as React from "react";
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Text, Badge } from "@chakra-ui/react";
 import { Genre } from "../../models/Genre";
 
 const GenreAttribute: React.FC<{ genre: Genre }> = ({ genre }) => {
   return (
-    <Box
-      maxHeight="30px"
-      maxWidth="6rem"
-      bgColor="var(--main-bg-color)"
-      color="white"
-      textAlign="center"
-      padding="5px 20px"
-      borderRadius="10px"
-    >
-      <Flex justifyContent="center" alignItems="center">
-        <Text fontSize="xs" fontWeight="bold" cursor="default">
-          {genre.name}
-        </Text>
-      </Flex>
-    </Box>
+    <Flex justifyContent="center" alignItems="center">
+      <Badge colorScheme="purple">{genre.name}</Badge>
+    </Flex>
   );
 };
 
