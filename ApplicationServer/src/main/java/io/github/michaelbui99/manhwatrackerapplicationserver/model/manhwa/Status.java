568f6d24f68173aca9eb7b84687fe4371ae6b2ab
Michael Bui <michaelbui99@hotmail.com>
2021-12-19 02:07:41 +0100

    Refractor applicationserver from java to typescript (nodejs)

--- ApplicationServer/src/main/java/io/github/michaelbui99/manhwatrackerapplicationserver/model/manhwa/Status.java
+++
@@ -1,5 +0,0 @@
-package io.github.michaelbui99.manhwatrackerapplicationserver.model.manhwa;
-
-public enum Status {
-    NOT_YET_RELEASED, RELEASING, FINISHED, HIATUS, CANCELLED
-}
--- ApplicationServer/src/main/java/io/github/michaelbui99/manhwatrackerapplicationserver/model/manhwarequest/RequestStatus.java
+++
@@ -1,7 +0,0 @@
-package io.github.michaelbui99.manhwatrackerapplicationserver.model.manhwarequest;
-
-public enum RequestStatus {
-    APPROVED,
-    REJECTED,
-    TBA
-}
