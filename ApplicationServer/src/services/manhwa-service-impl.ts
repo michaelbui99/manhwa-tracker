0c3bc173e2500bc2eea1a0d7a6a9fa4078305115
Michael Bui <michaelbui99@hotmail.com>
2021-12-19 22:44:09 +0100

    refractor manhwadao to also retrieve genres, tags and synonyms

--- ApplicationServer/src/services/manhwa-service-impl.ts
+++ ApplicationServer/src/services/manhwa-service-impl.ts
@@ -1,7 +1,6 @@
 import Manhwa from "src/models/manhwa";
 import { ManhwaService } from "./manhwa-service";
 import { ManhwaRepository } from "src/repositories/manhwa-repository";
-import { Service } from "typedi";
 
 export class ManhwaServiceImpl implements ManhwaService {
   constructor(private readonly manhwaRepository: ManhwaRepository) {}
