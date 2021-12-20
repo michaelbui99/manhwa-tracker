0b03c878665b6b7c0263e942f67738a1d36362a0
Michael Bui <michaelbui99@hotmail.com>
2021-12-20 01:04:20 +0100

    Refractor moved queries for tags, genres and synonyms to private method for better code reuse

--- ApplicationServer/src/dataaccess/manhwa/manhwa-dao-impl.ts
+++ ApplicationServer/src/dataaccess/manhwa/manhwa-dao-impl.ts
@@ -7,29 +7,20 @@ import { Tag } from "../models/tag";
 import { Synonym } from "src/models/synonym";
 
 export class ManhwaDAOImpl implements ManhwaDAO {
+  private baseDAO: BaseDAO;
+  private connection: Pool;
+
+  constructor() {
+    this.baseDAO = new BaseDAO();
+    this.connection = this.baseDAO.getConnection();
+  }
+
   async getAsync(id: number): Promise<Manhwa> {
-    const baseDAO = new BaseDAO();
-    const connection: Pool = baseDAO.getConnection();
-    const { rows }: { rows: Manhwa[] } = await connection.query(
+    const { rows }: { rows: Manhwa[] } = await this.connection.query(
       "SELECT * FROM manhwa WHERE id = $1",
       [id]
     );
-    const genres: QueryResult = await connection.query(
-      "select * from genre join manhwa_genre mg on genre.id = mg.genre_id where manhwa_id = $1",
-      [id]
-    );
-    const tags: QueryResult = await connection.query(
-      "select * from _tag join manhwa_tag mt on _tag.id = mt.tag_id where manhwa_id = $1",
-      [id]
-    );
-    const synonyms: QueryResult = await connection.query(
-      "select * from synonym join manhwa_synonym ms on synonym.title = ms.synonym_title and synonym.title_language = ms.synonym_title_language where manhwa_id = $1",
-      [id]
-    );
     const fetchedManhwa = rows[0];
-    const fetchedGenres: Genre[] = genres.rows;
-    const fetchedTags: Tag[] = tags.rows;
-    const fetchedSynonyms: Synonym[] = synonyms.rows;
     let manhwa = new Manhwa({
       id: fetchedManhwa.id,
       chapterCount: fetchedManhwa.id,
@@ -39,10 +30,10 @@ export class ManhwaDAOImpl implements ManhwaDAO {
       format: fetchedManhwa.format,
       sourceMaterial: fetchedManhwa.sourceMaterial,
       title: fetchedManhwa.title,
-      genres: fetchedGenres,
+      genres: await this.getGenresByManhwaId(fetchedManhwa.id),
       releaseDate: fetchedManhwa.releaseDate,
-      synonyms: fetchedSynonyms,
-      tags: fetchedTags,
+      synonyms: await this.getSynonymsByManhwaId(fetchedManhwa.id),
+      tags: await this.getTagsByManhwaId(fetchedManhwa.id),
     });
     return manhwa;
   }
@@ -54,5 +45,29 @@ export class ManhwaDAOImpl implements ManhwaDAO {
   }
   deleteAsync(id: number): void {
     throw new Error("Method not implemented.");
+  }
+
+  private async getGenresByManhwaId(id: number): Promise<Genre[]> {
+    const genres: QueryResult = await this.connection.query(
+      "select * from genre join manhwa_genre mg on genre.id = mg.genre_id where manhwa_id = $1",
+      [id]
+    );
+    return genres.rows;
+  }
+
+  private async getTagsByManhwaId(id: number): Promise<Tag[]> {
+    const tags: QueryResult = await this.connection.query(
+      "select * from _tag join manhwa_tag mt on _tag.id = mt.tag_id where manhwa_id = $1",
+      [id]
+    );
+    return tags.rows;
+  }
+
+  private async getSynonymsByManhwaId(id: number): Promise<Synonym[]> {
+    const synonyms: QueryResult = await this.connection.query(
+      "select * from synonym join manhwa_synonym ms on synonym.title = ms.synonym_title and synonym.title_language = ms.synonym_title_language where manhwa_id = $1",
+      [id]
+    );
+    return synonyms.rows;
   }
 }
