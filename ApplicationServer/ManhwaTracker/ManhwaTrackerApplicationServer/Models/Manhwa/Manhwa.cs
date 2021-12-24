b4dfc7d87b813e7e2c8be2723cd68d1bf3fa6349
Michael Bui <michaelbui99@hotmail.com>
2021-12-24 01:00:43 +0100

    Added new Model classes

---
+++ ApplicationServer/ManhwaTracker/ManhwaTrackerApplicationServer/Models/Manhwa/Manhwa.cs
@@ -0,0 +1,18 @@
+﻿namespace ManhwaTrackerApplicationServer.Models.Manhwa;
+
+public class Manhwa
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public string? Format { get; set; }
+    public Status Status { get; set; }
+    public SourceMaterial SourceMaterial { get; set; }
+    public DateTime? ReleaseDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public int ChapterCount { get; set; }
+    public string? CoverImage { get; set; }
+    public IEnumerable<Tag> Tags { get; set; }
+    public IEnumerable<Genre> Genres { get; set; }
+    public IEnumerable<Synonym> Synonyms { get; set; }
+}
---
+++ ApplicationServer/ManhwaTracker/ManhwaTrackerApplicationServer/Models/Manhwa/Tag.cs
@@ -0,0 +1,7 @@
+﻿namespace ManhwaTrackerApplicationServer.Models.Manhwa;
+
+public class Tag
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+}
