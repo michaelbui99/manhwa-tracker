68a8fe8c0e805379f05adef9770c97cda1077a3a
Michael Bui <michaelbui99@hotmail.com>
2021-12-26 22:49:07 +0100

    Implemented adding of tags, genres, release date and end date for RequestManhwa page

--- ReactManhwaTracker/manhwa-tracker/src/pages/request-manhwa/RequestManhwa.tsx
+++ ReactManhwaTracker/manhwa-tracker/src/pages/request-manhwa/RequestManhwa.tsx
@@ -8,6 +8,10 @@ import {
     Select,
     Textarea,
     VStack,
+    Text,
+    UnorderedList,
+    ListItem,
+    Button,
 } from "@chakra-ui/react";
 import * as React from "react";
 import { useState, useEffect } from "react";
@@ -17,10 +21,19 @@ import { Status } from "../../models/manhwa/status";
 import { Genre } from "../../models/manhwa/genre";
 import { Tag } from "../../models/manhwa/tag";
 
+// TODO: Refactor the form inputs into seperate components for better readability
+// TODO: Refactor into multi stage/step form to reduce how much the user have to scroll
+
 const RequestManhwa: React.FC = () => {
     const client = useApolloClient();
     const [genres, setGenres] = useState<Genre[]>([]);
     const [tags, setTags] = useState<Tag[]>([]);
+    const [addedGenres, setAddedGenres] = useState<Genre[]>([]);
+    const [addedTags, setAddedTags] = useState<Genre[]>([]);
+    const [selectedGenre, setSelectedGenre] = useState<Genre>();
+    const [selectedTag, setSelectedTag] = useState<Tag>();
+    const [releaseDate, setReleaseDate] = useState<Date>(new Date());
+    const [endDate, setEndDate] = useState<null | Date>(null);
     const GENRES_AND_TAGS = gql`
         {
             allGenres {
@@ -53,6 +66,19 @@ const RequestManhwa: React.FC = () => {
 
         resolveGenresAndTags();
     }, []);
+
+    const addGenre = () => {
+        if (genres && selectedGenre && !addedGenres.includes(selectedGenre)) {
+            setAddedGenres([...addedGenres, selectedGenre]);
+        }
+    };
+
+    const addTag = () => {
+        if (tags && selectedTag && !addedTags.includes(selectedTag)) {
+            setAddedTags([...addedTags, selectedTag]);
+            console.log("New tag added");
+        }
+    };
 
     return (
         <Box className="content">
@@ -98,6 +124,9 @@ const RequestManhwa: React.FC = () => {
                             Release Status
                         </FormLabel>
                         <Select variant="filled" boxShadow="md">
+                            <option disabled selected>
+                                -- select an option --
+                            </option>
                             {Object.values(Status).map((status) => (
                                 <option value={status}>{status}</option>
                             ))}
@@ -112,6 +141,9 @@ const RequestManhwa: React.FC = () => {
                             Source Material
                         </FormLabel>
                         <Select variant="filled" boxShadow="md">
+                            <option disabled selected>
+                                -- select an option --
+                            </option>
                             {Object.values(SourceMaterial).map(
                                 (sourceMaterial) => (
                                     <option value={sourceMaterial}>
@@ -123,6 +155,136 @@ const RequestManhwa: React.FC = () => {
                         <FormHelperText>
                             Choose what the source material of the manhwa is
                             based on
+                        </FormHelperText>
+
+                        {/* Genres */}
+                        <FormLabel fontSize="1.3rem" marginTop="2rem">
+                            Genre(s)
+                        </FormLabel>
+                        <Text fontSize="md">Added genres:</Text>
+                        {addedGenres.length > 0 ? (
+                            <UnorderedList>
+                                {addedGenres.map((g) => (
+                                    <ListItem>{g.name}</ListItem>
+                                ))}
+                            </UnorderedList>
+                        ) : (
+                            <Text fontSize="sm">
+                                No genres has been added yet
+                            </Text>
+                        )}
+
+                        <Select
+                            variant="filled"
+                            boxShadow="md"
+                            marginTop="0.5rem"
+                            onChange={(e) =>
+                                setSelectedGenre(
+                                    genres.find(
+                                        (g) =>
+                                            g.name.toLowerCase() ===
+                                            e.currentTarget.value.toLowerCase()
+                                    )
+                                )
+                            }
+                        >
+                            <option disabled selected>
+                                -- select an option --
+                            </option>
+                            {genres.map((genre) => (
+                                <option value={genre.name}>{genre.name}</option>
+                            ))}
+                        </Select>
+
+                        <Button onClick={addGenre} marginTop="0.6rem">
+                            Add
+                        </Button>
+                        <FormHelperText>
+                            Add genres for the Manhwa. Use the official genres
+                            defined by author / publisher if any is available.
+                            <br />
+                            Multiple genres can be added by selecting a genre
+                            and then press "Add"
+                        </FormHelperText>
+
+                        {/* Tags */}
+                        <FormLabel fontSize="1.3rem" marginTop="2rem">
+                            Tag(s)
+                        </FormLabel>
+                        <Text fontSize="md">Added Tags:</Text>
+                        {addedTags.length > 0 ? (
+                            <UnorderedList>
+                                {addedTags.map((t) => (
+                                    <ListItem>{t.name}</ListItem>
+                                ))}
+                            </UnorderedList>
+                        ) : (
+                            <Text fontSize="sm">
+                                No Tags has been added yet
+                            </Text>
+                        )}
+
+                        <Select
+                            variant="filled"
+                            boxShadow="md"
+                            marginTop="0.5rem"
+                            onChange={(e) =>
+                                setSelectedTag(
+                                    tags.find(
+                                        (t) =>
+                                            t.name.toLowerCase() ===
+                                            e.currentTarget.value.toLowerCase()
+                                    )
+                                )
+                            }
+                        >
+                            <option disabled selected>
+                                -- select an option --
+                            </option>
+                            {tags.map((tag) => (
+                                <option value={tag.name}>{tag.name}</option>
+                            ))}
+                        </Select>
+
+                        <Button onClick={addTag} marginTop="0.6rem">
+                            Add
+                        </Button>
+                        <FormHelperText>
+                            Add tags for the Manhwa. Use the official tags
+                            defined by author / publisher if any is available.
+                            <br />
+                            Multiple tags can be added by selecting a tags and
+                            then press "Add"
+                        </FormHelperText>
+
+                        {/* Release date */}
+                        <FormLabel fontSize="1.3rem" marginTop="2rem">
+                            Release date
+                        </FormLabel>
+                        <Input
+                            type="date"
+                            onChange={(e: any) =>
+                                setReleaseDate(e.target.valueAsDate)
+                            }
+                        />
+                        <FormHelperText>
+                            Pick the date of when the first chapter of the
+                            Manhwa was released
+                        </FormHelperText>
+
+                        {/* End date */}
+                        <FormLabel fontSize="1.3rem" marginTop="2rem">
+                            End date
+                        </FormLabel>
+                        <Input
+                            type="date"
+                            onChange={(e: any) =>
+                                setEndDate(e.target.valueAsDate)
+                            }
+                        />
+                        <FormHelperText>
+                            Pick the date of when the last chapter of the Manhwa
+                            was released if any
                         </FormHelperText>
                     </FormControl>
                 </Box>
