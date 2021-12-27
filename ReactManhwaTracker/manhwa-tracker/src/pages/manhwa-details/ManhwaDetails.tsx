f6509ef49a5ba841fc3fc010b8e3b19b5d42db1f
Michael Bui <michaelbui99@hotmail.com>
2021-12-28 00:57:15 +0100

    WIP ManhwaDetails page

--- ReactManhwaTracker/manhwa-tracker/src/pages/manhwa-details/ManhwaDetails.tsx
+++ ReactManhwaTracker/manhwa-tracker/src/pages/manhwa-details/ManhwaDetails.tsx
@@ -3,6 +3,10 @@ import { useState, useEffect } from "react";
 import { gql, useApolloClient } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import Manhwa from "../../models/manhwa/manhwa";
+import { Box, Heading, HStack, Image, Text } from "@chakra-ui/react";
+import GenreAttribute from "../../components/genre-attribute/GenreAttribute";
+import TagAttribute from "../../components/tag-attribute/TagAttribute";
+import { Status } from "../../models/manhwa/status";
 
 const ManhwaDetails: React.FC = () => {
     const [manhwa, setManhwa] = useState<Manhwa | null>();
@@ -58,7 +62,93 @@ const ManhwaDetails: React.FC = () => {
         resolveManhwa();
     }, []);
 
-    return <div>{manhwa ? <p>{manhwa.title}</p> : ""}</div>;
+    return (
+        <Box className="content">
+            <Box boxShadow="md" minHeight="30vh" padding="2rem 1rem">
+                <HStack>
+                    <Image
+                        display={{ base: "none", md: "block" }}
+                        maxWidth={{ base: "100px", md: "200px", lg: "300px" }}
+                        maxHeight={{ base: "150px", md: "250px", lg: "350px" }}
+                        paddingRight="3rem"
+                        src={
+                            manhwa
+                                ? manhwa.coverImage
+                                    ? manhwa.coverImage
+                                    : ""
+                                : ""
+                        }
+                    />
+
+                    <Box>
+                        <Heading fontSize="2xl">
+                            {manhwa ? `${manhwa.title}` : ""}
+                        </Heading>
+                        {/* Chapter count  */}
+                        <Text marginTop="1rem">
+                            Chapter count: {manhwa ? manhwa.chapterCount : "0"}
+                        </Text>
+
+                        {/* Release date */}
+                        <Text>
+                            Release date:
+                            {manhwa
+                                ? manhwa.status === Status.RELEASING ||
+                                  manhwa.status === Status.CANCELLED ||
+                                  manhwa.status === Status.HIATUS ||
+                                  manhwa.status === Status.FINISHED
+                                    ? manhwa.releaseDate
+                                    : ""
+                                : ""}
+                        </Text>
+
+                        {/* End date if manhwa is finished or cancelled */}
+                        {manhwa
+                            ? manhwa.status === Status.FINISHED ||
+                              manhwa.status === Status.CANCELLED
+                                ? manhwa.endDate
+                                : ""
+                            : ""}
+
+                        {/* Description */}
+                        <Text marginTop="1rem">
+                            {manhwa ? manhwa.description : ""}
+                        </Text>
+
+                        {/* All genres if any */}
+                        {manhwa ? (
+                            manhwa?.genres.length > 0 ? (
+                                <HStack marginTop="1rem">
+                                    {manhwa.genres.map((g) => (
+                                        <GenreAttribute genre={g} />
+                                    ))}
+                                </HStack>
+                            ) : (
+                                ""
+                            )
+                        ) : (
+                            ""
+                        )}
+
+                        {/* All tags if any */}
+                        {manhwa ? (
+                            manhwa?.tags.length > 0 ? (
+                                <HStack marginTop="1rem">
+                                    {manhwa.tags.map((t) => (
+                                        <TagAttribute tag={t} />
+                                    ))}
+                                </HStack>
+                            ) : (
+                                ""
+                            )
+                        ) : (
+                            ""
+                        )}
+                    </Box>
+                </HStack>
+            </Box>
+        </Box>
+    );
 };
 
 export default ManhwaDetails;
