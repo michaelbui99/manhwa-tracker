ed07ebcc707ab628ad1dbaddbe64acdb5613139a
Michael Bui <michaelbui99@hotmail.com>
2022-01-05 12:34:19 +0100

    Added switching back to development branch again after merging + pushing

--- merge_dev_and_push.py
+++ merge_dev_and_push.py
@@ -1,21 +1,31 @@
 import subprocess
 
 
+def print_subprocess_output(subprocess):
+    print(subprocess.stdout)
+    print(subprocess.stderr)
+
+
+# Check current branch
 current_branch = subprocess.run(
     ['git', 'rev-parse', '--abbrev-ref', 'HEAD'], shell=True, capture_output=True, text=True)
 
-print(current_branch.stdout)
-print(current_branch.stderr)
+print_subprocess_output(current_branch)
 
+# checkout main if on dev branch
 if (current_branch.stdout.strip() == "development"):
     checkout_main = subprocess.run(['git', 'checkout', 'main'], shell=True,
                                    capture_output=True, text=True)
+    print_subprocess_output(checkout_main)
 
-print(checkout_main.stdout)
-print(checkout_main.stderr)
 
+# merge main with dev branch and push
 merge_dev_branch_and_push = subprocess.run(
     ['git', 'merge', 'development', '--no-ff', '&&', 'git', 'push'], shell=True, capture_output=True, text=True)
 
 print(merge_dev_branch_and_push.stdout)
 print(merge_dev_branch_and_push.stderr)
+
+# switch back to dev branch to continue work
+checkout_dev = subprocess.run(
+    ['git', 'checkout', 'development'], shell=True, capture_output=True, text=True)
