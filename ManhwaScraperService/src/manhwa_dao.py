6f7d6ae6a2f86cdb34423a794f6d3a4849387902
Michael Bui <michaelbui99@hotmail.com>
2022-01-28 21:57:15 +0100

    Removed unused dependency from Dockerfile and ManhwaScraper

--- ManhwaScraperService/src/manhwa_dao.py
+++ ManhwaScraperService/src/manhwa_dao.py
@@ -1,66 +1,66 @@
-import psycopg2
-from psycopg2.extras import RealDictCursor
-from dotenv import dotenv_values
+# import psycopg2
+# from psycopg2.extras import RealDictCursor
+# from dotenv import dotenv_values
 
 
-class ManhwaDAO:
-    def __init__(self):
-        """ Creates a new ManhwaDAO instance
+# class ManhwaDAO:
+#     def __init__(self):
+#         """ Creates a new ManhwaDAO instance
 
-            Method reads all database details 
-            from .env file
-        """
-        self.config = dotenv_values(".env")
+#             Method reads all database details
+#             from .env file
+#         """
+#         self.config = dotenv_values(".env")
 
-    def create_connection(self):
-        """Creates a new connection to database
+#     def create_connection(self):
+#         """Creates a new connection to database
 
-           Method database credentials are fetched from self.config  
-        """
+#            Method database credentials are fetched from self.config
+#         """
 
-        connection = psycopg2.connect(
-            host=self.config["DB_HOST"],
-            database=self.config["DB_DB"],
-            user=self.config["DB_USER"],
-            password=self.config["DB_PW"],
-            cursor_factory=RealDictCursor
-        )
-        return connection
+#         connection = psycopg2.connect(
+#             host=self.config["DB_HOST"],
+#             database=self.config["DB_DB"],
+#             user=self.config["DB_USER"],
+#             password=self.config["DB_PW"],
+#             cursor_factory=RealDictCursor
+#         )
+#         return connection
 
-    def get_genres(self):
-        """ Queries all genres """
+#     def get_genres(self):
+#         """ Queries all genres """
 
-        with (self.create_connection()) as connection:
-            try:
-                cursor = connection.cursor()
-                cursor.execute('SELECT * FROM "public"."Genres"')
+#         with (self.create_connection()) as connection:
+#             try:
+#                 cursor = connection.cursor()
+#                 cursor.execute('SELECT * FROM "public"."Genres"')
 
-                genres = cursor.fetchall()
-                return genres
-            except(Exception, psycopg2.DatabaseError) as error:
-                print(error)
+#                 genres = cursor.fetchall()
+#                 return genres
+#             except(Exception, psycopg2.DatabaseError) as error:
+#                 print(error)
 
-    def get_manhwas(self):
-        """Queries all Manhwas"""
-        with (self.create_connection()) as connection:
-            try:
-                cursor = connection.cursor()
-                cursor.execute('SELECT * FROM "public"."Manhwas"')
+#     def get_manhwas(self):
+#         """Queries all Manhwas"""
+#         with (self.create_connection()) as connection:
+#             try:
+#                 cursor = connection.cursor()
+#                 cursor.execute('SELECT * FROM "public"."Manhwas"')
 
-                manhwas = cursor.fetchall()
-                return manhwas
-            except(Exception, psycopg2.DatabaseError) as error:
-                print(error)
+#                 manhwas = cursor.fetchall()
+#                 return manhwas
+#             except(Exception, psycopg2.DatabaseError) as error:
+#                 print(error)
 
-    def get_manhwa_by_id(self, id):
-        """Queries a single Manhwa by id"""
-        with (self.create_connection()) as connection:
-            try:
-                cursor = connection.cursor()
-                cursor.execute(
-                    'SELECT * FROM "public"."Manhwas" where "Id"= %s;', (str(id)))
+#     def get_manhwa_by_id(self, id):
+#         """Queries a single Manhwa by id"""
+#         with (self.create_connection()) as connection:
+#             try:
+#                 cursor = connection.cursor()
+#                 cursor.execute(
+#                     'SELECT * FROM "public"."Manhwas" where "Id"= %s;', (str(id)))
 
-                manhwa = cursor.fetchone()
-                return manhwa
-            except(Exception, psycopg2.DatabaseError) as error:
-                print(error)
+#                 manhwa = cursor.fetchone()
+#                 return manhwa
+#             except(Exception, psycopg2.DatabaseError) as error:
+#                 print(error)
