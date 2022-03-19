572a1f6f204f0d25af1dfcc9393777ba46b5901d
Michael Bui <michaelbui99@hotmail.com>
2022-03-19 18:19:00 +0100

    Added Dockerfile for ManhwaScraper

--- ManhwaScraper/src/test/java/io/github/michaelbui99/manhwascraper/ManhwaScraperApplicationTests.java
+++ ManhwaScraper/src/test/java/io/github/michaelbui99/manhwascraper/ManhwaScraperApplicationTests.java
@@ -1,13 +1,13 @@
-package io.github.michaelbui99.manhwascraper;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ManhwaScraperApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package io.github.michaelbui99.manhwascraper;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ManhwaScraperApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
