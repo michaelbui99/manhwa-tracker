1b66da85748270dd927c40fe484588cbf8db1add
Michael Bui <michaelbui99@hotmail.com>
2022-04-02 18:17:33 +0200

    Login page styling

--- ManhwaTrackerWebsite/manhwa-tracker/pages/login.tsx
+++ ManhwaTrackerWebsite/manhwa-tracker/pages/login.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Box, Button, Fade, Link, TextField, Typography } from "@mui/material";
 
 const Login = () => {
-    return <div>login</div>;
+    const [pageLoad, setPageLoad] = useState(false);
+
+    useEffect(() => {
+        setPageLoad(true);
+    }, []);
+
+    return (
+        <>
+            <Fade in={pageLoad}>
+                <Box
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                    flexGrow="1"
+                    marginTop="10vh"
+                >
+                    <Box
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="flex-start"
+                        flexDirection="column"
+                        width="400px"
+                        rowGap="2rem"
+                    >
+                        <Typography variant="h2" fontSize="24px">
+                            Login
+                        </Typography>
+                        <TextField label="Email" fullWidth />
+                        <TextField label="Password" fullWidth />
+                        <Typography>
+                            Don't have an account?{" "}
+                            <Link href="/signup">Sign up here</Link>
+                        </Typography>
+                        <Button variant="contained" sx={{ marginLeft: "auto" }}>
+                            Login
+                        </Button>
+                    </Box>
+                </Box>
+            </Fade>
+        </>
+    );
 };
 
 export default Login;
