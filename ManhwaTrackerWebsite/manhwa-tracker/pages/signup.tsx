5d80fa1e712d8029c8f7219969ef9e29da7fb827
Michael Bui <michaelbui99@hotmail.com>
2022-04-15 18:01:30 +0200

    Styling of signup page

--- ManhwaTrackerWebsite/manhwa-tracker/pages/signup.tsx
+++ ManhwaTrackerWebsite/manhwa-tracker/pages/signup.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import { Box, Button, Fade, Link, TextField, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
 
 const SignUp = () => {
-    return <div>signup</div>;
+    const [pageLoad, setPageLoad] = useState(false);
+
+    useEffect(() => {
+        setPageLoad(true);
+    }, []);
+
+    return (
+        <>
+            <Fade in={pageLoad}>
+                <Box
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                    flexGrow="1"
+                    marginTop="2vh"
+                >
+                    <Box
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="flex-start"
+                        flexDirection="column"
+                        width="400px"
+                        rowGap="2rem"
+                    >
+                        <Typography variant="h2" fontSize="24px">
+                            Sign up
+                        </Typography>
+                        <TextField label="Email" fullWidth />
+                        <TextField label="Confirm Email" fullWidth />
+                        <TextField label="Password" fullWidth />
+                        <TextField label="Confirm Password" fullWidth />
+                        <Typography>
+                            Already have an account?{" "}
+                            <Link href="/login">Login here</Link>
+                        </Typography>
+                        <Button variant="contained" sx={{ marginLeft: "auto" }}>
+                            Sign up
+                        </Button>
+                    </Box>
+                </Box>
+            </Fade>
+        </>
+    );
 };
 
 export default SignUp;
